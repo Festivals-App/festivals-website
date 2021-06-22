0f8da207d6e6932c98a7c9822e7d9e4277c4294e
Simon Gaus <phisto05@gmail.com>
2021-06-23 01:08:55 +0200

    Migrate from apple to bash script

--- compile.sh
+++ compile.sh
@@ -9,7 +9,6 @@ mkdir -p "$build_path"
 cp "$source_path"/* "$build_path"
 
 
-
 css_source_path="$source_path/css"
 css_build_path="$build_path/css"
 temp_css_file="$build_path/css/temp_css_file.css"
@@ -22,7 +21,6 @@ rm "$temp_css_file"
 
 
 
-
 php_source_path="$source_path/php"
 en_source_path="$source_path/en"
 images_source_path="$source_path/images"
@@ -30,9 +28,6 @@ images_source_path="$source_path/images"
 cp -r "$php_source_path" "$build_path"
 cp -r "$en_source_path" "$build_path"
 cp -r "$images_source_path" "$build_path"
-
-
-
 
 rm -rf "$current_build_path"
 mkdir -p "$current_build_path"
