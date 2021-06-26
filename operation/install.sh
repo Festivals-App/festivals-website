c7df2496082667685aeff26cddd31d7e9efee20c
Simon Gaus <phisto05@gmail.com>
2021-06-26 20:51:01 +0200

    Refactor install and update process

--- operation/install.sh
+++ operation/install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 #
 # install.sh 1.0.0
-#
 # 
-#
 # (c)2020-2021 Simon Gaus
 #
 
 # Move to working directory
 #
 cd /usr/local || exit
+
+# Check for nginx
+# 
+if ! command -v nginx > /dev/null; then
+  echo "NGINX is not installed. Please setup the environment properly before running this script. Exiting."
+  exit 1
+fi
 
 # Install minify if needed.
 #
@@ -25,16 +30,29 @@ if ! command -v git > /dev/null; then
   apt-get install git -y > /dev/null;
 fi
 
-# Compiling and installing festivals-website to /var/www/festivalsapp.org
+# Install curl if needed.
 #
-echo "Downloading current festivals-website..."
-yes | sudo git clone https://github.com/Festivals-App/festivals-website.git /usr/local/festivals-website > /dev/null;
-cd /usr/local/festivals-website
-./compile.sh
-rm -rf /var/www/festivalsapp.org
-chown -R $USER:$USER /var/www/festivalsapp.org
-chmod -R 755 /var/www/festivalsapp.org
+if ! command -v curl > /dev/null; then
+  echo "Installing curl..."
+  apt-get install curl -y > /dev/null;
+fi
 
-# Cleanup
-cd ~/
-rm -rf /usr/local/festivals-website
+# Use the upgrade script to instal newest website
+#
+curl -o update.sh https://raw.githubusercontent.com/Festivals-App/festivals-website/master/operation/update.sh > /dev/null;
+chmod +x update.sh
+./update.sh
+rm update.sh
+
+# Install the nginx config file
+#
+systemctl stop nginx
+echo "Installing the nginx configuration file"
+mkdir -p /usr/local/festivals-website
+cd /usr/local/festivals-website || exit
+curl -o nginx-config https://raw.githubusercontent.com/Festivals-App/festivals-website/master/operation/nginx-config > /dev/null;
+cp nginx-config /etc/nginx/sites-available/festivalsapp.org
+ln -s /etc/nginx/sites-available/festivalsapp.org /etc/nginx/sites-enabled/
+systemctl start nginx
+
+echo "Now you need to setup ssl and you are ready to go."
--- operation/update.sh
+++ operation/update.sh
@@ -1,22 +1,49 @@
 #!/bin/bash
 #
-# update.sh 1.0.0
-#
-#
-#
+# install.sh 1.0.0
+# 
 # (c)2020-2021 Simon Gaus
 #
+
+# Move to working directory
+#
+cd /usr/local || exit
+
+# Check for nginx
+# 
+if ! command -v nginx > /dev/null; then
+  echo "NGINX is not installed. Please setup the environment properly before running this script. Exiting."
+  exit 1
+fi
 
 # Check for minify
 # 
 if ! command -v minify > /dev/null; then
-  echo "Minify is not installed. Exiting."
+  echo "MINIFY is not installed. Please run the install script first. Exiting."
   exit 1
 fi
 
-# Check for find
+# Check for git
 # 
-if ! command -v find > /dev/null; then
-  echo "Find is not installed. Exiting."
+if ! command -v git > /dev/null; then
+  echo "GIT is not installed. Please run the install script first. Exiting."
   exit 1
 fi
+
+# Compiling and installing festivals-website to /var/www/festivalsapp.org
+#
+echo "Downloading current festivals-website..."
+yes | sudo git clone https://github.com/Festivals-App/festivals-website.git /usr/local/festivals-website > /dev/null;
+cd /usr/local/festivals-website
+./compile.sh
+
+echo "Installing current festivals-website..."
+systemctl stop nginx
+rm -rf /var/www/festivalsapp.org
+chown -R $USER:$USER /var/www/festivalsapp.org
+chmod -R 755 /var/www/festivalsapp.org
+
+# Cleanup
+cd /usr/local || exit
+rm -rf /usr/local/festivals-website
+systemctl start nginx
