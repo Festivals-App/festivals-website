294a53fba216e62946dd05d7cac66970170a7ace
Simon Gaus <phisto05@gmail.com>
2022-03-25 11:39:20 +0100

    Update supdate script

--- operation/update.sh
+++ operation/update.sh
@@ -2,50 +2,62 @@
 #
 # install.sh 1.0.0
 # 
-# (c)2020-2021 Simon Gaus
+# (c)2020-2022 Simon Gaus
 #
 
-# Move to working directory
+# Check for NGINX
 #
-cd /usr/local || exit
-
-# Check for nginx
-# 
 if ! command -v nginx > /dev/null; then
-  echo "NGINX is not installed. Please setup the environment properly before running this script. Exiting."
+  echo "NGINX is not installed. Exiting."
   exit 1
 fi
 
-# Check for minify
-# 
-if ! command -v minify > /dev/null; then
-  echo "MINIFY is not installed. Please run the install script first. Exiting."
-  exit 1
-fi
-
-# Check for git
-# 
-if ! command -v git > /dev/null; then
-  echo "GIT is not installed. Please run the install script first. Exiting."
-  exit 1
-fi
-
-# Compiling and installing festivals-website to /var/www/festivalsapp.org
+# Move to working dir
 #
-echo "Downloading current festivals-website..."
-yes | sudo git clone https://github.com/Festivals-App/festivals-website.git /usr/local/festivals-website > /dev/null;
-cd /usr/local/festivals-website
-./compile.sh
+mkdir /usr/local/festivals-website || { echo "Failed to create working directory. Exiting." ; exit 1; }
+cd /usr/local/festivals-website || { echo "Failed to access working directory. Exiting." ; exit 1; }
 
-echo "Installing current festivals-website..."
+echo "Installing festivals-website"
+sleep 1
+
+# Download and extract the latest website release
+#
+echo "Downloading latest festivals-website files"
+sleep 1
+file_url="https://github.com/Festivals-App/festivals-website/releases/latest/download/festivals-website.tar.gz"
+curl -L "$file_url" -o festivals-website.tar.gz
+tar -xzvf festivals-website.tar.gz
+rm festivals-website.tar.gz
+rm nginx-config
+
+# Stop nginx if running
+#
+echo "Stopping NGINX"
+sleep 1
 systemctl stop nginx
+
+# Install the website files
+#
+echo "Copying the festivals-website files"
+sleep 1
 rm -rf /var/www/festivalsapp.org
-mv /usr/local/festivals-website/build/current /var/www/festivalsapp.org
+mkdir -p /var/www/festivalsapp.org
+cp -a ./. /var/www/festivalsapp.org/
 chown -R $USER:$USER /var/www/festivalsapp.org
 chmod -R 755 /var/www/festivalsapp.org
 
-# Cleanup
-cd /usr/local || exit
-rm -rf /usr/local/festivals-website
+# Start nginx
+#
+echo "Starting NGINX"
+sleep 1
 systemctl start nginx
-echo "Done installing the newest festivals-website."
+
+# Delete working dir
+#
+echo "Cleanup..."
+cd /usr/local || exit
+rm -R /usr/local/festivals-website
+sleep 1
+
+echo "Now you need to setup ssl and you are ready to go."
+sleep 1
